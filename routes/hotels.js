ef244bb1bb1292e871ae94923004646eb7083d2c
aravinda-coder <aravindabandara84@gmail.com>
2023-05-04 12:42:06 +0530

    hotel-update-api

--- routes/hotels.js
+++ routes/hotels.js
@@ -3,32 +3,12 @@ let Hotel = require('../models/hotel.model');
 
 router.route('/').get((req, res) => {
     Hotel.find()
-    .then(hotels => res.json(flights))
+    .then(hotels => res.json(hotels))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
-/*
-  const departure_destination = req.body.departure_destination;
-  const arrival_destination = req.body.arrival_destination;
-  const departure_date = Date.parse(req.body.departure_date);
-  const arrival_date = Date.parse(req.body.arrival_date);
-  const cabin_class = req.body.cabin_class;
-  const airline = req.body.airline;
-  const price = Number(req.body.price);
-  const duration = req.body.duration;
-  const window_seat_count = Number(req.body.window_seat_count);
-  const midddle_seat_count = Number(req.body.midddle_seat_count);
-  */
-  
-  /*
-  hotel_name:{type: String,required:true},
-  destination: {type: String, required:true},
-  checkInDate:{type:Date,required:true},
-  checkOutDate:{type:Date,required:true},
-  price:{type:Number,required:true}
-  
-  */
+
 
   const hotel_name = req.body.hotel_name;
   const destination = req.body.destination;
@@ -63,19 +43,22 @@ router.route('/:id').delete((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/update/:id').post((req, res) => {
-    Hotel.findById(req.params.id)
-    .then(exercise => {
-      exercise.username = req.body.username;
-      exercise.description = req.body.description;
-      exercise.duration = Number(req.body.duration);
-      exercise.date = Date.parse(req.body.date);
-
-      exercise.save()
-        .then(() => res.json('Flight updated!'))
-        .catch(err => res.status(400).json('Error: ' + err));
-    })
-    .catch(err => res.status(400).json('Error: ' + err));
+router.route('/update/:id').put((req, res) => {
+    let hotelId = req.params.id;
+    const {hotel_name,destination,checkInDate,checkOutDate,price} = req.body;
+    const updateStudent = {
+        hotel_name,
+        destination,
+        checkInDate,
+        checkOutDate,
+        price 
+    }
+   const update = Hotel.findByIdAndUpdate(hotelId,updateStudent)
+   .then(() => {
+    res.status(200).send({status: "hotel updated"})
+   }).catch((err) =>{
+    console.log(err)
+   })
 });
 
 module.exports = router;
